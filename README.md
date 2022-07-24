27b8abd0c717d7c719ac7fb2a81faf283c2c258d
dollarkillerx <adapawang@gmail.com>
2022-07-24 22:54:38 +0800

    init

--- README.md
+++ README.md
@@ -1857,3 +1857,29 @@ void main() {
   print(json.decode(strData));
 }
 ```
+
+
+#### 设置沉浸式状态栏(Status Bar)
+
+https://xuyisheng.top/flutter_uioverlay/
+
+``` 
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+
+void main(){
+  runApp(MyApp());
+
+  if(Platform.isAndroid){
+    SystemUiOverlayStyle style = SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      ///这是设置状态栏的图标和字体的颜色 
+      ///Brightness.light  一般都是显示为白色
+      ///Brightness.dark 一般都是显示为黑色
+      statusBarIconBrightness: Brightness.light
+    );
+    SystemChrome.setSystemUIOverlayStyle(style);
+  }
+}
+```
--- flutter/demo1/l1完結/main.dart
+++ flutter/demo1/l1完結/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:helloworld/pages/Tabls.dart';
 import 'routes/Routes.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+
+import 'dart:io';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
+  if(Platform.isAndroid){
+    SystemUiOverlayStyle style = SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        ///这是设置状态栏的图标和字体的颜色
+        ///Brightness.light  一般都是显示为白色
+        ///Brightness.dark 一般都是显示为黑色
+        statusBarIconBrightness: Brightness.light
+    );
+    SystemChrome.setSystemUIOverlayStyle(style);
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -14,7 +26,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // 關閉debug 圖標
       home: Tabls(),
-      initialRoute: "/HTTPPage",
+      // initialRoute: "/HTTPPage",
       onGenerateRoute: onGenerateRoute, // 沒有router才會執行 onGenerateRoute
       // 國際化
       localizationsDelegates: [
